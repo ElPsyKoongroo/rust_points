ed476f75eb6b814a68d2d14129821d83c040f2be
sergious234 <sergious234@gmail.com>
2023-07-17 15:10:52 +0200

    Destroying C++

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use rand::Rng;
 
+const FIXED_POINTS: usize = 6;
+
 use std::{
     cmp::Ordering,
     f64::MAX,
@@ -57,6 +59,182 @@ impl Ord for Punto {
     }
 }
 
+struct DyV<'a> {
+    puntos: &'a [Punto],
+    best_option: BestPoint,
+}
+
+impl<'a> DyV<'a> {
+    pub fn new(puntos: &'a [Punto]) -> DyV {
+        DyV {
+            puntos,
+            best_option: BestPoint {
+                distancia: MAX,
+                points: [0; 3],
+            },
+        }
+    }
+
+    pub fn start(&mut self) -> &BestPoint {
+        self.divide_venceras(self.puntos[0].x, self.puntos[self.puntos.len() - 1].x);
+
+        return &self.best_option;
+    }
+
+    fn calcula_fixed(&mut self, start: usize, end: usize) -> ExhaustiveResult {
+        if end - start + 1 < 3 {
+            return ExhaustiveResult::Nothing;
+        }
+        let end = end + 1;
+
+        let mut actual_option = BestPoint {
+            distancia: MAX,
+            points: [0; 3],
+        };
+
+        let mut distancia;
+        for (i, punto_i) in (start..end).zip(self.puntos[start..end].iter()) {
+            for (j, punto_j) in (start..end).zip(self.puntos[start..end].iter()) {
+                if j == i {
+                    continue;
+                }
+
+                for (k, punto_k) in ((i + 1)..end).zip(self.puntos[i + 1..end].iter()) {
+                    if j == k {
+                        continue;
+                    }
+
+                    distancia = punto_i.distancia3(punto_j, punto_k);
+
+                    if distancia < actual_option.distancia {
+                        actual_option.distancia = distancia;
+                        actual_option.points = [i, j, k];
+                    }
+                }
+            }
+        }
+        if actual_option.distancia < self.best_option.distancia {
+            self.best_option.distancia = actual_option.distancia;
+            self.best_option.points = actual_option.points;
+            return ExhaustiveResult::Found;
+        }
+
+        return ExhaustiveResult::NotFound(actual_option); //best_option.clone();
+    }
+
+    fn divide_venceras(&mut self, start: f64, end: f64) -> ExhaustiveResult {
+        let mitad: f64 = (start + end) / 2.0;
+        let (start_index, end_index) = get_points_between(self.puntos, start, end);
+
+        /*
+        if end_index - start_index + 1 < 3 {
+            return ExhaustiveResult::Nothing;
+        }
+        */
+
+        if end_index - start_index + 1 < 3 || repes(self.puntos, start_index, end_index) {
+            return ExhaustiveResult::Nothing;
+        }
+
+        if end_index - start_index + 1 <= FIXED_POINTS {
+            return self.calcula_fixed(start_index, end_index);
+        }
+
+        let izq = self.divide_venceras(start, mitad);
+        let drc = self.divide_venceras(mitad, end);
+
+        let distancia_minima = match (izq, drc) {
+            (ExhaustiveResult::Nothing, ExhaustiveResult::Nothing) => {
+                return self.calcula_fixed(start_index, end_index)
+            }
+            (ExhaustiveResult::Found, _) => self.recheck_actual_best(end, start, mitad), //best_option.clone(),
+            (_, ExhaustiveResult::Found) => self.recheck_actual_best(end, start, mitad),
+            (ExhaustiveResult::NotFound(mut r_izq), ExhaustiveResult::NotFound(mut r_drc)) => {
+                if r_izq.distancia < r_drc.distancia {
+                    recheck(self.puntos, &mut r_izq, end, start, mitad)
+                } else {
+                    recheck(self.puntos, &mut r_drc, end, start, mitad)
+                }
+            }
+            _ => self.recheck_actual_best(end, start, mitad),
+        };
+
+        match distancia_minima {
+            ExhaustiveResult::Found => return ExhaustiveResult::Found,
+            ExhaustiveResult::NotFound(a) => return ExhaustiveResult::NotFound(a),
+            ExhaustiveResult::NothingRecheck(a) => {
+                match self.calcula_fixed(start_index, end_index) {
+                    ExhaustiveResult::Found => return ExhaustiveResult::Found,
+                    ExhaustiveResult::NotFound(aux) => {
+                        if aux.distancia < a.distancia {
+                            return ExhaustiveResult::NotFound(aux);
+                        } else {
+                            return ExhaustiveResult::NotFound(a);
+                        };
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn recheck_actual_best(&mut self, end: f64, start: f64, mitad: f64) -> ExhaustiveResult {
+        if self.best_option.distancia < end - start {
+            let (new_start, new_end) = get_points_between(
+                self.puntos,
+                mitad - self.best_option.distancia,
+                mitad + self.best_option.distancia,
+            );
+
+            match self.calcula_fixed(new_start, new_end) {
+                ExhaustiveResult::Found => return ExhaustiveResult::Found,
+                ExhaustiveResult::NotFound(aux) => {
+                    if aux.distancia < self.best_option.distancia {
+                        return ExhaustiveResult::NotFound(aux);
+                    } else {
+                        return ExhaustiveResult::Found;
+                    };
+                }
+                _ => unreachable!(),
+            };
+        }
+
+        ExhaustiveResult::NothingRecheck(self.best_option.clone())
+    }
+}
+
+/*
+fn get_points_between(puntos: &[Punto], start: f64, end: f64) -> (usize, usize) {
+    let start_index = puntos
+        .iter()
+        .position(|&p| p.x > start)
+        .unwrap_or_else(|| N_POINTS); // 722
+    let end_index = puntos.iter().rposition(|&p| p.x < end).unwrap();
+    (start_index, end_index)
+}
+*/
+
+fn get_points_between(puntos: &[Punto], start: f64, end: f64) -> (usize, usize) {
+    let start_index = match puntos.binary_search_by(|p| p.x.partial_cmp(&start).unwrap()) {
+        Ok(index) => index,
+        Err(index) => index,
+    };
+    let end_index = match puntos.binary_search_by(|p| p.x.partial_cmp(&end).unwrap()) {
+        Ok(index) => index,
+        Err(index) => index - 1,
+    };
+    (start_index, end_index)
+}
+
+fn repes(puntos: &[Punto], start: usize, end: usize) -> bool {
+    return (start..end - 1).all(|i| puntos[i].x == puntos[i + 1].x);
+}
+
+//fn repes(puntos: &[Punto], start: usize, end: usize) -> bool {
+//    puntos[start..end - 1].windows(2).all(|window| window[0].x == window[1].x)
+//}
+
 fn calcula_fixed(
     puntos: &[Punto],
     start: usize,
@@ -106,7 +284,7 @@ fn calcula_fixed(
         }
         i += 1;
     }*/
-    
+
     let mut distancia;
     for i in start..end {
         for j in start..end {
@@ -136,6 +314,7 @@ fn calcula_fixed(
 
     return ExhaustiveResult::NotFound(actual_option); //best_option.clone();
 }
+/*
 
 fn get_points_between(puntos: &[Punto], start: f64, end: f64) -> (usize, usize) {
     let start_index = puntos
@@ -226,7 +405,7 @@ fn divide_venceras(
         _ => unreachable!(),
     }
 }
-
+*/
 fn recheck(
     puntos: &[Punto],
     distancia_minima: &mut BestPoint,
@@ -302,20 +481,24 @@ fn read_points_from_file(file_name: &str) -> Vec<Punto> {
     points
 }
 
-static N_POINTS: usize = 30_000;
-static MEDIA: u128 = 30;
+static N_POINTS: usize = 90_000;
+static MEDIA: u128 = 10;
 
 fn main() {
-    //let mut puntos = genera_random(N_POINTS, 800.0, 0.0);
-    //puntos.sort();
-    //write_points(&puntos);
+    /*
+    let mut puntos = genera_random(N_POINTS, 800.0, 0.0);
+    puntos.sort();
+    write_points(&puntos);
+    */
+
     let puntos = read_points_from_file("puntos.tsp");
     println!("GO!");
     let puntos = puntos;
     let mut media = 0;
     for _ in 0..MEDIA {
+        let mut dyv = DyV::new(&puntos);
         let start = Instant::now();
-        let res = divide_benceras(&puntos);
+        let res = dyv.start();
         let end = Instant::now();
 
         println!("{:?}", res);
