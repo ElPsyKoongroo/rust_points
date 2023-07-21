9fdacb100b8853b0f83dd3aec5185bc2b71f98c3
sergious234 <sergious234@gmail.com>
2023-07-21 21:18:03 +0200

    Better code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use rand::Rng;
 
-const FIXED_POINTS: usize = 24;
+const FIXED_POINTS: usize = 30;
 
 use std::{
     cmp::Ordering,
-    f64::MAX,
     io::{BufRead, Write},
     time::Instant,
 };
+
+const MAX: f64 = f64::MAX;
 
 #[derive(Clone, Debug, Default, Copy)]
 struct Punto {
@@ -16,14 +17,6 @@ struct Punto {
 }
 
 type BestPoint = f64;
-
-#[derive(Clone, Debug)]
-enum ExhaustiveResult {
-    NotFound(BestPoint),
-    Found,
-    Nothing,
-    NothingRecheck,
-}
 
 impl Punto {
     #[inline]
@@ -39,16 +32,19 @@ impl Punto {
 }
 
 impl PartialOrd for Punto {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         self.x.partial_cmp(&other.x)
     }
 }
+
 impl PartialEq for Punto {
     fn eq(&self, other: &Self) -> bool {
         self.x == other.x
     }
 }
+
 impl Eq for Punto {}
+
 impl Ord for Punto {
     fn cmp(&self, other: &Self) -> Ordering {
         self.x.total_cmp(&other.x)
@@ -70,16 +66,19 @@ impl<'a> DyV<'a> {
         }
     }
 
-    pub fn start(&mut self) -> &BestPoint {
+    pub fn start(&mut self) -> BestPoint {
         self.divide_venceras(self.puntos[0].x, self.puntos[self.puntos.len() - 1].x);
 
-        return &self.best_option;
+        self.best_option
     }
-    fn calcula_fixed(&mut self, start: usize, end: usize) -> ExhaustiveResult {
+
+    fn calcula_fixed(&mut self, start: usize, end: usize) {
+        assert!(end <= self.puntos.len());
         let mut actual_option = self.best_option;
         let mut distancia: f64;
 
         let mut points: [usize; 3] = [0, 0, 0];
+
         for (i, punto_i) in (start..end).zip(self.puntos[start..end].iter()) {
             for (j, punto_j) in (start..end).zip(self.puntos[start..end].iter()) {
                 let distancia_ij = punto_i.distancia(punto_j);
@@ -87,7 +86,11 @@ impl<'a> DyV<'a> {
                     continue;
                 }
 
-                for (k, punto_k) in (i + 1..end).zip(self.puntos[i + 1..end].iter()) {
+                let iter_k = (i + 1..end)
+                    .zip(self.puntos[i + 1..end].iter())
+                    .filter(|(e, _)| *e != j);
+
+                for (k, punto_k) in iter_k {
                     if j == k {
                         continue;
                     }
@@ -105,105 +108,37 @@ impl<'a> DyV<'a> {
         if actual_option < self.best_option {
             self.best_option = actual_option;
             self.points = points;
-            return ExhaustiveResult::Found;
         }
-
-        return ExhaustiveResult::NotFound(self.best_option);
     }
 
-    fn divide_venceras(&mut self, start: f64, end: f64) -> ExhaustiveResult {
+    fn divide_venceras(&mut self, start: f64, end: f64) {
         let (start_index, end_index) = get_points_between(self.puntos, start, end);
 
-        if end_index - start_index + 1 < 3 || repes(self.puntos, start_index, end_index) {
-            return ExhaustiveResult::Nothing;
-        }
-
-        if end_index - start_index + 1 <= FIXED_POINTS {
-            return self.calcula_fixed(start_index, end_index);
+        if end_index - start_index < FIXED_POINTS {
+            self.calcula_fixed(start_index, end_index + 1);
+            return;
         }
 
         let mitad: f64 = (start + end) / 2.0;
-        let izq = self.divide_venceras(start, mitad);
-        let drc = self.divide_venceras(mitad, end);
+        self.divide_venceras(start, mitad);
+        self.divide_venceras(mitad, end);
 
-        let distancia_minima = match (izq, drc) {
-            (ExhaustiveResult::Nothing, ExhaustiveResult::Nothing) => self.recheck(end, start),
-
-            (ExhaustiveResult::Found, _) | (_, ExhaustiveResult::Found) => {
-                self.recheck_actual_best(end, start)
-            }
-
-            _ => self.recheck_actual_best(end, start),
-        };
-
-        match distancia_minima {
-            ExhaustiveResult::Found => return ExhaustiveResult::Found,
-            ExhaustiveResult::NotFound(_) => return ExhaustiveResult::Nothing,
-            ExhaustiveResult::NothingRecheck => {
-                match self.calcula_fixed(start_index, end_index + 1) {
-                    ExhaustiveResult::Found => return ExhaustiveResult::Found,
-                    _ => return ExhaustiveResult::NotFound(self.best_option),
-                }
-            }
-            _ => unreachable!(),
-        }
-    }
-
-    fn recheck_actual_best(&mut self, end: f64, start: f64) -> ExhaustiveResult {
-        let mitad: f64 = (start + end) / 2.0;
         if self.best_option < end - start {
-            let (new_start, new_end) = get_points_between(
-                self.puntos,
-                mitad - self.best_option,
-                mitad + self.best_option,
-            );
-
-            match self.calcula_fixed(new_start, new_end + 1) {
-                ExhaustiveResult::Found | ExhaustiveResult::NotFound(_) => {
-                    return ExhaustiveResult::Found
-                }
-                /*
-                ExhaustiveResult::NotFound(_) => return ExhaustiveResult::Found,
-
-                    if aux < self.best_option {
-                        return ExhaustiveResult::NotFound(aux);
-                    } else {
-                        return ExhaustiveResult::Found;
-                    };
-                }
-                    */
-                _ => unreachable!(),
-            };
-        }
-
-        ExhaustiveResult::NothingRecheck
+            self.recheck_actual_best(end, start);
+        } else {
+            self.calcula_fixed(start_index, end_index + 1);
+        };
     }
 
-    fn recheck(&mut self, end: f64, start: f64) -> ExhaustiveResult {
+    fn recheck_actual_best(&mut self, end: f64, start: f64) {
         let mitad: f64 = (start + end) / 2.0;
-        let distancia_minima = self.best_option;
-        if distancia_minima < end - start {
-            let (new_start, new_end) = get_points_between(
-                self.puntos,
-                mitad - distancia_minima,
-                mitad + distancia_minima,
-            );
-            match self.calcula_fixed(new_start, new_end + 1) {
-                ExhaustiveResult::Found => return ExhaustiveResult::Found,
-                /*
-                ExhaustiveResult::NotFound(aux) => {
-                    if aux < distancia_minima {
-                        return ExhaustiveResult::NotFound(aux);
-                    } else {
-                        return ExhaustiveResult::Found;
-                    };
-                }
-                */
-                _ => unreachable!(),
-            };
-        }
+        let (new_start, new_end) = get_points_between(
+            self.puntos,
+            mitad - self.best_option,
+            mitad + self.best_option,
+        );
 
-        ExhaustiveResult::NothingRecheck
+        self.calcula_fixed(new_start, new_end + 1);
     }
 }
 
@@ -217,10 +152,6 @@ fn get_points_between(puntos: &[Punto], start: f64, end: f64) -> (usize, usize) 
         Err(index) => index - 1,
     };
     (start_index, end_index)
-}
-
-fn repes(puntos: &[Punto], start: usize, end: usize) -> bool {
-    return (start..end - 1).all(|i| puntos[i].x == puntos[i + 1].x);
 }
 
 #[allow(unused)]
@@ -254,11 +185,10 @@ fn read_points_from_file(file_name: &str) -> Vec<Punto> {
     while buffer.trim() != "NODE_COORD_SECTION" {
         buffer.clear();
         reader.read_line(&mut buffer).unwrap();
-        println!("{}", buffer);
     }
 
     while reader.read_line(&mut buffer).unwrap() != 0 {
-        let values: Vec<&str> = buffer.trim().split(" ").collect();
+        let values: Vec<&str> = buffer.trim().split(' ').collect();
         points.push(Punto {
             x: values[1].parse().unwrap(),
             y: values[2].parse().unwrap(),
@@ -270,7 +200,18 @@ fn read_points_from_file(file_name: &str) -> Vec<Punto> {
 }
 
 static N_POINTS: usize = 800_000;
-static MEDIA: u128 = 20;
+static MEDIA: u128 = 1;
+
+#[allow(unused)]
+fn write_points_with_name(name: String, puntos: &[Punto]) {
+    let mut file = std::io::BufWriter::new(std::fs::File::create(name).unwrap());
+    file.write_all("NODE_COORD_SECTION\n".as_bytes()).unwrap();
+
+    for punto in puntos {
+        file.write_all(format!("1 {} {}\n", punto.x, punto.y).as_bytes())
+            .unwrap();
+    }
+}
 
 fn main() {
     /*
@@ -279,20 +220,109 @@ fn main() {
     write_points(&puntos);
     */
 
+    for i in 0..10 {
+        let puntos = read_points_from_file(&format!("point_files/puntos_rand_{}.tsp", i));
+        let mut dyv = DyV::new(&puntos);
+        let res = dyv.start();
+        println!("{:?}", res);
+    }
+
+    /*
     let puntos = read_points_from_file("puntos.tsp");
     println!("GO!");
-    //let puntos = puntos;
     let mut media = 0;
     for _ in 0..MEDIA {
         let mut dyv = DyV::new(&puntos);
+
         let start = Instant::now();
         let res = dyv.start();
         let end = Instant::now();
 
-        println!("{:?}", res);
+        println!("{:?}, {:?}", res, dyv.points);
+
+        for index in dyv.points {
+            println!("{:?}", puntos[index]);
+        }
         println!("{}", end.duration_since(start).as_millis());
 
         media += end.duration_since(start).as_millis();
     }
-    println!("Media: {}", media / MEDIA);
+    println!("Media: {} ms", media / MEDIA);
+    */
+}
+
+#[cfg(test)]
+mod tests {
+    use super::read_points_from_file;
+    use super::DyV;
+
+    #[test]
+    fn test_1() {
+        let puntos = read_points_from_file("point_files/puntos_800000.tsp");
+        let mut dyv = DyV::new(&puntos);
+        let res = dyv.start();
+        assert_eq!(res, 0.03781606866923333);
+    }
+
+    #[test]
+    fn test_2() {
+        let puntos = read_points_from_file("point_files/puntos_500000.tsp");
+        let mut dyv = DyV::new(&puntos);
+        let res = dyv.start();
+        assert_eq!(res, 0.05537844995897111);
+    }
+
+    // [30_000, 50_000, 80_000, 150_000,
+    #[test]
+    fn test_3() {
+        let puntos = read_points_from_file("point_files/puntos_150000.tsp");
+        let mut dyv = DyV::new(&puntos);
+        let res = dyv.start();
+        assert_eq!(res, 0.144052106804805);
+    }
+    #[test]
+    fn test_4() {
+        let puntos = read_points_from_file("point_files/puntos_80000.tsp");
+        let mut dyv = DyV::new(&puntos);
+        let res = dyv.start();
+        assert_eq!(res, 0.322497589363708);
+    }
+
+    #[test]
+    fn test_5() {
+        let puntos = read_points_from_file("point_files/puntos_50000.tsp");
+        let mut dyv = DyV::new(&puntos);
+        let res = dyv.start();
+        assert_eq!(res, 0.30805303890637226);
+    }
+    #[test]
+    fn test_6() {
+        let puntos = read_points_from_file("point_files/puntos_30000.tsp");
+        let mut dyv = DyV::new(&puntos);
+        let res = dyv.start();
+        assert_eq!(res, 0.27095504920223346);
+    }
+
+    #[test]
+    fn random_tests() {
+        let answers = [
+            0.08015304030013183,
+            0.05353786710188051,
+            0.08537619304985208,
+            0.05132736906745347,
+            0.02924335899771857,
+            0.045820223238880894,
+            0.04419541844017895,
+            0.0990423678758528,
+            0.05702137712944691,
+            0.033750012246621455,
+        ];
+
+        for i in 0..10 {
+            let puntos = read_points_from_file(&format!("point_files/puntos_rand_{}.tsp", i));
+            let mut dyv = DyV::new(&puntos);
+            let res = dyv.start();
+            assert_eq!(res, answers[i]);
+        }
+    }
 }
